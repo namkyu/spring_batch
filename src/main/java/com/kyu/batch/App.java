4d1a4d5b17e0f2f9a06e9b55af742cf0c40eaf85
이남규 <lng1982@gmail.com>
2016-05-18 17:21:12 +0900

    test code 수정

--- src/main/java/com/kyu/batch/App.java
+++ src/main/java/com/kyu/batch/App.java
@@ -1,9 +1,18 @@
 package com.kyu.batch;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -16,20 +25,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 
-	public static void main(String[] args) {
+	@Test
+	public void 배치테스트() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
 		String[] springConfig = { "classpath:spring/batch/jobs/job-hello-world.xml" };
 		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
 
 		JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
 		Job job = context.getBean("helloWorldJob", Job.class);
+		JobExecution execution = jobLauncher.run(job, new JobParameters());
 
-		try {
-			JobExecution execution = jobLauncher.run(job, new JobParameters());
-			System.out.println("Exit Status : " + execution.getStatus());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("Done");
+		assertThat(BatchStatus.COMPLETED, is(execution.getStatus()));
 	}
 }
